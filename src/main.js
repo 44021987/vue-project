c1f2a61537903c56c570bd8034bc48f3f320c9ff
44021987 <44021987@qq.com>
2017-07-19 10:39:42 +0800

    axios 替换 vue-resource

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import VueResource from 'vue-resource'
+import Axios from 'axios'
+//import VueResource from 'vue-resource'
 import App from './App'
 import router from './router'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import 'font-awesome/css/font-awesome.css'
 
-Vue.use(VueResource);
+Vue.prototype.$http = Axios;
+//Vue.use(VueResource);
 Vue.use(Element);
 Vue.config.productionTip = false
 
